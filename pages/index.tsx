30a01fa9086c47fcceb032ada308b542321ab164
surendhar-1706 <surendhar2k18@gmail.com>
2022-02-27 23:50:06 +0530

    seo

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,10 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <Box>
+      <Head>
+        <title>Surendhar Palanisamy</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <MainHome />
     </Box>
   )
