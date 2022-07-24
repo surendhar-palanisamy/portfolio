c0e2580d922f325ae4eb67394b774200c983775c
surendhar-palanisamy <surendhar2k18@gmail.com>
2022-07-24 19:45:24 +0530

    added cryptoverse

--- components/Projects/Instagram.tsx
+++ components/Projects/Instagram.tsx
@@ -12,78 +12,12 @@ import Image from 'next/image';
 import InstaPic from '../../public/insta3.jpg'
 import UpworkPic from '../../public/upwork.jpg'
 import GptPic from '../../public/Headerportfolio.png';
-
+import Cryptoverse from '../../public/cryptoverse.jpg'
 function Instagram() {
     return (
 
         <VStack>
             <Grid templateColumns={['repeat(1  , 1fr)', 'repeat(1  , 1fr)', 'repeat(1  , 1fr)', 'repeat(2  , 1fr)']} gap={4}>
-                <GridItem data-aos-duration="600"
-                    data-aos-easing="ease-in-out" data-aos-once="true" data-aos="zoom-in-up" py={2} shadow='xl' rounded={'lg'} borderWidth={'thin'}  >
-                    <Box alignItems={'flex-end'} gap={4} display={['', '', 'flex', 'flex']} px={'4'} py={2}  >
-                        <VStack   >
-                            <Image alt="Picture of project" src={InstaPic} />
-
-                            <HStack>
-                                <Link isExternal href='https://insta-mauve.vercel.app/' _hover={{ decoration: 'none' }}>
-                                    <Button size={'sm'} leftIcon={<CgWebsite />}  > Demo</Button>
-                                </Link>
-                                <Link isExternal href='https://github.com/surendhar-palanisamy/insta' _hover={{ decoration: 'none' }}>
-                                    <Button size={'sm'} leftIcon={<FaGithubAlt />} >Github</Button></Link>
-                            </HStack>
-
-                        </VStack>
-                        <Stack  >
-                            <Text fontWeight={'semibold'} fontSize={'xl'}>Instagram Clone</Text>
-                            <Text w={['', '', '300px', '300px']}>
-                                The idea of this website is to create a usable copy of a popular media sharing app  using a serverless backend having a realtime EventListeners and
-                                Providing the end user with app monitoring functionalities
-
-                            </Text>
-                            <Grid w={['', '', '300px', '300px']} gap={3} templateColumns='repeat(3, 0fr)'>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>ReactJs</TagLabel>
-                                    </Tag>
-                                </GridItem>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>NextJs</TagLabel>
-                                    </Tag>
-                                </GridItem>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>TypeScript</TagLabel>
-                                    </Tag>
-                                </GridItem>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>Firebase</TagLabel>
-                                    </Tag>
-                                </GridItem>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>OAuth</TagLabel>
-                                    </Tag>
-                                </GridItem>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>ChakraUI</TagLabel>
-                                    </Tag>
-                                </GridItem>
-                                <GridItem >
-                                    <Tag colorScheme='teal'>
-                                        <TagLabel>Recoil</TagLabel>
-                                    </Tag>
-                                </GridItem>
-
-
-
-
-                            </Grid>
-                        </Stack>
-                    </Box>
-                </GridItem>
 
                 <GridItem data-aos-duration="600"
                     data-aos-easing="ease-in-out" data-aos-once="true" data-aos="zoom-in-up" py={2} shadow='xl' rounded={'lg'} borderWidth={'thin'}  >
@@ -152,6 +86,152 @@ function Instagram() {
                         </Stack>
                     </Box>
                 </GridItem>
+
+
+                <GridItem data-aos-duration="600"
+                    data-aos-easing="ease-in-out" data-aos-once="true" data-aos="zoom-in-up" py={2} shadow='xl' rounded={'lg'} borderWidth={'thin'}  >
+                    <Box alignItems={'flex-end'} gap={4} display={['', '', 'flex', 'flex']} px={'4'} py={2}  >
+                        <VStack   >
+                            <Image alt="Picture of project" src={InstaPic} />
+
+                            <HStack>
+                                <Link isExternal href='https://insta-mauve.vercel.app/' _hover={{ decoration: 'none' }}>
+                                    <Button size={'sm'} leftIcon={<CgWebsite />}  > Demo</Button>
+                                </Link>
+                                <Link isExternal href='https://github.com/surendhar-palanisamy/insta' _hover={{ decoration: 'none' }}>
+                                    <Button size={'sm'} leftIcon={<FaGithubAlt />} >Github</Button></Link>
+                            </HStack>
+
+                        </VStack>
+                        <Stack  >
+                            <Text fontWeight={'semibold'} fontSize={'xl'}>Instagram Clone</Text>
+                            <Text w={['', '', '300px', '300px']}>
+                                The idea of this website is to create a usable copy of a popular media sharing app  using a serverless backend having a realtime EventListeners and
+                                Providing the end user with app monitoring functionalities
+
+                            </Text>
+                            <Grid w={['', '', '300px', '300px']} gap={3} templateColumns='repeat(3, 0fr)'>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>ReactJs</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>NextJs</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>TypeScript</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>Firebase</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>OAuth</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>ChakraUI</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>Recoil</TagLabel>
+                                    </Tag>
+                                </GridItem>
+
+
+
+
+                            </Grid>
+                        </Stack>
+                    </Box>
+                </GridItem>
+                {/* cryptoverse */}
+                <GridItem data-aos-duration="600"
+                    data-aos-easing="ease-in-out" data-aos-once="true" data-aos="zoom-in-up" py={2} shadow='xl' rounded={'lg'} borderWidth={'thin'}  >
+                    <Box alignItems={'flex-end'} gap={4} display={['', '', 'flex', 'flex']} px={'4'} py={2}  >
+                        <VStack gap={[0, 0, 12, 12]}  >
+                            <Image placeholder="blur" alt="Picture of project" height={250} width={250} src={Cryptoverse} />
+
+                            <HStack>
+                                <Link isExternal href='https://cryptoverse-lyart.vercel.app/' _hover={{ decoration: 'none' }}>
+                                    <Button size={'sm'} leftIcon={<CgWebsite />}  > Demo</Button>
+                                </Link>
+                                <Link isExternal href='https://github.com/surendhar-palanisamy/cryptoverse' _hover={{ decoration: 'none' }}>
+                                    <Button size={'sm'} leftIcon={<FaGithubAlt />} >Github</Button></Link>
+                            </HStack>
+
+                        </VStack>
+                        <Stack  >
+                            <Text fontWeight={'semibold'} fontSize={'xl'}>Cryptoverse</Text>
+                            <Text w={['', '', '300px', '300px']}>
+                                One stop solution to all your cryptcurrency related questions Realtime time Update about your favourite cryptocurrency Historical chart data A library of all the listed coin and much more..
+
+                            </Text>
+                            <Grid w={['', '', '300px', '300px']} gap={3} templateColumns='repeat(3, 0fr)'>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>ReactJs</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>NextJs</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>TypeScript</TagLabel>
+                                    </Tag>
+                                </GridItem>
+
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>ChakraUI</TagLabel>
+                                    </Tag>
+                                </GridItem>
+
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>Vercel</TagLabel>
+                                    </Tag>
+                                </GridItem>
+
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>AI</TagLabel>
+                                    </Tag>
+                                </GridItem>
+
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>Rapid API</TagLabel>
+                                    </Tag>
+                                </GridItem>
+
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>Chart.js</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                                <GridItem >
+                                    <Tag colorScheme='teal'>
+                                        <TagLabel>Redux ToolKit</TagLabel>
+                                    </Tag>
+                                </GridItem>
+                            </Grid>
+                        </Stack>
+                    </Box>
+                </GridItem>
+
                 <GridItem
                     data-aos-duration="600"
                     data-aos-easing="ease-in-out"
